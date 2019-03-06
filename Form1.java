e81ada90b194b45d8a5317bbbc091a00c543b8d1
sireeshasiri1 <psireeshareddy35@gmail.com>
2019-03-06 12:05:13 +0530

    First program

---
+++ Form1.java
@@ -0,0 +1,11 @@
+class Form1{
+       public static void main(String args[]){
+        System.out.println("this is java program");
+        System.out.println("this is python program");
+        System.out.println("this is jenkins program");
+        System.out.println("this is azure program");
+        System.out.println("this is python-jenkins program");
+        System.out.println("this is yaml with python program");
+        System.out.println("this is python with azure program");
+        }
+   }
